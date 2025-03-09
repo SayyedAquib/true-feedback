c628c38c1e7933b3018c21cce0f66d71c3ef706a
SayyedAquib <sayyed.aquib89@gmail.com>
2025-03-09 16:01:35 +0530

    Add sample .env file, update .gitignore, and implement database connection logic

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to database!!");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+    console.log("db: ", db);
+
+    connection.isConnected = db.connections[0].readyState;
+    console.log("db.connections: ", db.connections);
+
+    console.log("DB connected successfully!!");
+  } catch (error) {
+    console.error("Database connection failed: ", error);
+    process.exit(1);
+  }
+}
